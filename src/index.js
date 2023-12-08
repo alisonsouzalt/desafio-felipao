c9f84a8fdd52b94bceef785686375960feb4854f
alisonsouzalt <alisonsouzalt@hotmail.com>
2023-12-08 13:17:09 -0300

    continuação

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-console.log("oi")
+// inicio do projeto //
+
+let nomeHeroi = "Lord Palladins"
+let xp = 0
+
+for(let ferro = 1; ferro <= 1000; ferro++){
+    xp += 1
+    console.log("Seu nível de XP é: " + ferro)
+}
+
+console.log("Parabéns!! Você chegou ao nível de Ferro. " + "XP: " + xp)
+
+for(let bronze = 1001; bronze <= 2000; bronze++){
+    xp += 1
+    console.log("Seu nível de XP é: " + bronze)
+}
+
+console.log("Parabéns!! Você chegou ao nível de Bronze. " + "XP: " + xp)
+
+for(let prata = 2001; prata <= 5000; prata++){
+    xp += 1
+    console.log("Seu nível de XP é: " + prata)
+}
+
+console.log("Parabéns!! Você chegou ao nível de Prata. " + "XP: " + xp)
+
+for(let ouro = 6001; ouro <= 7000; ouro++){
+    xp += 1
+    console.log("Seu nível de XP é: " + ouro)
+}
+
+console.log("Parabéns!! Você chegou ao nível de Ouro. " + "XP: " + xp)
+
+for(let platina = 7001; platina <= 8000; platina++){
+    xp += 1
+    console.log("Seu nível de XP é: " + platina)
+}
+
+console.log("Parabéns!! Você chegou ao nível de Platina. " + "XP: " + xp)
+
+for(let ascendente = 8001; ascendente <= 9000; ascendente++){
+    xp += 1
+    console.log("Seu nível de XP é: " + ascendente)
+}
+
+console.log("Parabéns!! Você chegou ao nível de Ascendente. " + "XP: " + xp)
+
+for(let imortal = 9001; imortal <= 10000; imortal++){
+    xp += 1
+    console.log("Seu nível de XP é: " + imortal)
+}
+
+console.log("Parabéns!! Você chegou ao nível de Imortal. " + "XP: " + xp)
+
+for(let radiante = 10001; radiante <= 11000; radiante++){
+    xp += 1
+    console.log("Seu nível de XP é: " + radiante)
+}
+
+console.log("Parabéns!! Você chegou ao nível de Radiante. " + "XP: " + xp)
