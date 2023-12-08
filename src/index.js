bda6d0e49d3b6d3636fb05d73a8581492143e723
alisonsouzalt <alisonsouzalt@hotmail.com>
2023-12-08 19:48:39 -0300

    the end

--- src/index.js
+++ src/index.js
@@ -1,60 +1,98 @@
-// inicio do projeto //
+// inicio do projeto da DIO//
 
 let nomeHeroi = "Lord Palladins"
 let xp = 0
 
+// o código abaixo foi criado para indicar em qual nível o herói está começando, assim se repetindo a cada nível alcançado //
+console.log("O Héroi de nome " + nomeHeroi + " está no nível de Ferro")
+
 for(let ferro = 1; ferro <= 1000; ferro++){
     xp += 1
-    console.log("Seu nível de XP é: " + ferro)
+    console.log("Seu héroi está com " + ferro + " de XP")
 }
 
-console.log("Parabéns!! Você chegou ao nível de Ferro. " + "XP: " + xp)
-
-for(let bronze = 1001; bronze <= 2000; bronze++){
+// abaixo criei meio que uma repetição para indicar que após o héroi obter 1001 de xp ele alcança o nível de bronze //
+for(let bronze = 1001; bronze <= 1001; bronze++){
     xp += 1
-    console.log("Seu nível de XP é: " + bronze)
+    console.log("Seu héroi está com " + bronze + " de XP")
 }
 
-console.log("Parabéns!! Você chegou ao nível de Bronze. " + "XP: " + xp)
+console.log("O Héroi de nome " + nomeHeroi + " está no nível de Bronze")
 
-for(let prata = 2001; prata <= 5000; prata++){
+// em seguida todo o siclo se repete, até chegar no nível máximo //
+for(let bronze = 1002; bronze <= 2000; bronze++){
     xp += 1
-    console.log("Seu nível de XP é: " + prata)
+    console.log("Seu héroi está com " + bronze + " de XP")
 }
 
-console.log("Parabéns!! Você chegou ao nível de Prata. " + "XP: " + xp)
-
-for(let ouro = 6001; ouro <= 7000; ouro++){
+for(let prata = 2001; prata <= 2001; prata++){
     xp += 1
-    console.log("Seu nível de XP é: " + ouro)
+    console.log("Seu héroi está com " + prata + " de XP")
 }
 
-console.log("Parabéns!! Você chegou ao nível de Ouro. " + "XP: " + xp)
+console.log("O Héroi de nome " + nomeHeroi + " está no nível de Prata")
 
-for(let platina = 7001; platina <= 8000; platina++){
+for(let prata = 2002; prata <= 6000; prata++){
     xp += 1
-    console.log("Seu nível de XP é: " + platina)
+    console.log("Seu héroi está com " + prata + " de XP")
 }
 
-console.log("Parabéns!! Você chegou ao nível de Platina. " + "XP: " + xp)
-
-for(let ascendente = 8001; ascendente <= 9000; ascendente++){
+for(let ouro = 6001; ouro <= 6001; ouro++){
     xp += 1
-    console.log("Seu nível de XP é: " + ascendente)
+    console.log("Seu héroi está com " + ouro + " de XP")
 }
 
-console.log("Parabéns!! Você chegou ao nível de Ascendente. " + "XP: " + xp)
+console.log("O Héroi de nome " + nomeHeroi + " está no nível de Ouro")
 
-for(let imortal = 9001; imortal <= 10000; imortal++){
+for(let ouro = 6002; ouro <= 7000; ouro++){
     xp += 1
-    console.log("Seu nível de XP é: " + imortal)
+    console.log("Seu héroi está com " + ouro + " de XP")
 }
 
-console.log("Parabéns!! Você chegou ao nível de Imortal. " + "XP: " + xp)
-
-for(let radiante = 10001; radiante <= 11000; radiante++){
+for(let platina = 7001; platina <= 7001; platina++){
     xp += 1
-    console.log("Seu nível de XP é: " + radiante)
+    console.log("Seu héroi está com " + platina + " de XP")
 }
 
-console.log("Parabéns!! Você chegou ao nível de Radiante. " + "XP: " + xp)
+console.log("O Héroi de nome " + nomeHeroi + " está no nível de Platina")
+
+for(let platina = 7002; platina <= 8000; platina++){
+    xp += 1
+    console.log("Seu héroi está com " + platina + " de XP")
+}
+
+for(let ascendente = 8001; ascendente <= 8001; ascendente++){
+    xp += 1
+    console.log("Seu héroi está com " + ascendente + " de XP")
+}
+
+console.log("O Héroi de nome " + nomeHeroi + " está no nível de Ascendente")
+
+for(let ascendente = 8002; ascendente <= 9000; ascendente++){
+    xp += 1
+    console.log("Seu héroi está com " + ascendente + " de XP")
+}
+
+for(let imortal = 9001; imortal <= 9001; imortal++){
+    xp += 1
+    console.log("Seu héroi está com " + imortal + " de XP")
+}
+
+console.log("O Héroi de nome " + nomeHeroi + " está no nível de Imortal")
+
+for(let imortal = 9002; imortal <= 10000; imortal++){
+    xp += 1
+    console.log("Seu héroi está com " + imortal + " de XP")
+}
+
+for(let radiante = 10001; radiante <= 10001; radiante++){
+    xp += 1
+    console.log("Seu héroi está com " + radiante + " de XP")
+}
+
+console.log("O Héroi de nome " + nomeHeroi + " está no nível de Radiante")
+
+for(let radiante = 10001; radiante >= 10001; radiante++){
+    xp += 1
+    console.log("Seu héroi está com " + radiante + " de XP")
+}
